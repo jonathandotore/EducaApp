385e5bec6b01e40d877d18b1dcafa32db9e9e290
Jonathan Dotore <jonathandotoree@outlook.com>
2026-09-02 15:35:04 -0300

    [ADD] Adicionado tratamento de exceções

---
+++ EducaWebApi.Domain/Exceptions/DatabaseException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EducaWebApi.Domain.Exceptions
+{
+    public class DatabaseException : Exception
+    {
+        public DatabaseException(string mensagem, Exception innerException)
+            : base(mensagem, innerException)
+        {
+        }
+    }
+}
