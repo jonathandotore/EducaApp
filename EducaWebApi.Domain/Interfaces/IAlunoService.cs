99bbf5b4029dcb0e09ef9ba3f2144d60ce668c25
Jonathan Dotore <jonathandotoree@outlook.com>
2026-09-02 12:35:58 -0300

    [ADD] Adição de interface para camada de Service da entidade de Alunos

---
+++ EducaWebApi.Domain/Interfaces/IAlunoService.cs
@@ -0,0 +1,14 @@
+﻿using EducaWebApi.Domain.Dtos;
+using EducaWebApi.Domain.Entities;
+
+namespace EducaWebApi.Domain.Interfaces
+{
+    public interface IAlunoService
+    {
+        ResultadosPaginados<Aluno> ListarPaginado(string nomeFiltro, int pagina, int tamanhoPagina);
+        Aluno ObterPorId(int id);
+        Aluno Criar(Aluno aluno);
+        Aluno Atualizar(int id, Aluno aluno);
+        void Inativar(int id);
+    }
+}
