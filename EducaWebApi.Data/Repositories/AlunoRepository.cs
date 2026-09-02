7f3fcd2c3b4c9f4912babdbb4b9d00e9b1af8ff2
Jonathan Dotore <jonathandotoree@outlook.com>
2026-09-02 15:33:57 -0300

    [ADD] Adição das camadas de serviço e repositório para separação da regra de negócio do acesso ao dados. IAlunoService apenas lida com as regras e IAlunoRepository lida apenas com a obtenção dos dados já filtrados

---
+++ EducaWebApi.Data/Repositories/AlunoRepository.cs
@@ -0,0 +1,99 @@
+﻿using Dapper;
+using EducaWebApi.Data.Connection;
+using EducaWebApi.Domain.Dtos;
+using EducaWebApi.Domain.Entities;
+using EducaWebApi.Domain.Exceptions;
+using EducaWebApi.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace EducaWebApi.Data.Repositories
+{
+    public class AlunoRepository : IAlunoRepository
+    {
+        private readonly IConnectionFactory _connectionFactory;
+
+        public AlunoRepository(IConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public Task<ResultadosPaginados<AlunoResponseDto>> ListarPaginado(string nomeFiltro, int pagina, int tamanhoPagina)
+        {
+            return ExecutarAsync(async () =>
+            {
+                const string sqlItens = @"
+                    SELECT Id, Nome, Email, DataNascimento, Ativo, DataCadastro
+                    FROM Aluno
+                    WHERE (@NomeFiltro IS NULL OR Nome LIKE '%' + @NomeFiltro + '%')
+                    ORDER BY Nome
+                    OFFSET @Offset ROWS FETCH NEXT @TamanhoPagina ROWS ONLY;";
+
+                const string sqlTotal = @"
+                    SELECT COUNT(*)
+                    FROM Aluno
+                    WHERE (@NomeFiltro IS NULL OR Nome LIKE '%' + @NomeFiltro + '%');";
+
+                using (var conexao = _connectionFactory.CreateConnection())
+                {
+                    var offset = (pagina - 1) * tamanhoPagina;
+
+                    var itens = await conexao.QueryAsync<AlunoResponseDto>(sqlItens, new
+                    {
+                        NomeFiltro = nomeFiltro,
+                        Offset = offset,
+                        TamanhoPagina = tamanhoPagina
+                    });
+
+                    var total = await conexao.ExecuteScalarAsync<int>(sqlTotal, new { NomeFiltro = nomeFiltro });
+
+                    return new ResultadosPaginados<AlunoResponseDto>
+                    {
+                        Itens = new List<AlunoResponseDto>(itens),
+                        TotalRegistros = total,
+                        Pagina = pagina,
+                        TamanhoPagina = tamanhoPagina
+                    };
+                }
+            }, "Erro na tentativa de listar alunos.");
+        }
+
+        public async Task<AlunoResponseDto> ObterPorId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Aluno> Atualizar(int id, Aluno aluno)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Aluno> Criar(Aluno aluno)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Aluno> Inativar(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #region Métodos privados
+
+        private async Task<T> ExecutarAsync<T>(Func<Task<T>> operacao, string mensagemErro)
+        {
+            try
+            {
+                return await operacao();
+            }
+            catch (SqlException ex)
+            {
+                throw new DatabaseException(mensagemErro, ex);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ EducaWebApi.Domain/Dtos/AlunoResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EducaWebApi.Domain.Dtos
+{
+    public class AlunoResponseDto
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime DataNascimento { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
