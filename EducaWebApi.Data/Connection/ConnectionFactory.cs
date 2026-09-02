336e742f48fede9c0acff73bfc22a79391b8d3c3
Jonathan Dotore <jonathandotoree@outlook.com>
2026-09-02 12:33:59 -0300

    [ADD] Criando arquivo de configuração para abstração das instâncias de conexão com o banco de dados

---
+++ EducaWebApi.Data/Connection/ConnectionFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EducaWebApi.Data.Connection
+{
+    public interface IConnectionFactory
+    {
+        IDbConnection CreateConnection();
+    }
+
+    public class SqlConnectionFactory : IConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public SqlConnectionFactory()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["EducaWebApiConnection"].ConnectionString;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
