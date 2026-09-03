96f02eada50648e8790a680f2f2d0f09e05f10d6
Jonathan Dotore <jonathandotoree@outlook.com>
2026-09-03 16:21:33 -0300

    [MOD] Alterada string de conexão com o redis na classe origem.

--- EducaWebApi.Data/Cache/RedisConnectionFactory.cs
+++ EducaWebApi.Data/Cache/RedisConnectionFactory.cs
@@ -8,7 +8,7 @@ namespace EducaWebApi.Data.Cache
     {
         private static readonly Lazy<ConnectionMultiplexer> _conexao = new Lazy<ConnectionMultiplexer>(() =>
         {
-            var connectionString = ConfigurationManager.AppSettings["RedisConnection"] ?? "localhost:6379,abortConnect=false";
+            var connectionString = ConfigurationManager.AppSettings["RedisConnection"] ?? "localhost:6379,abortConnect=false,connectTimeout=2500,connectRetry=1,syncTimeout=2500";
             return ConnectionMultiplexer.Connect(connectionString);
         });
 
