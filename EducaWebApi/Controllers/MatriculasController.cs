83960475dba6fa328e3e80e4e7c661e4a7a34c2d
Jonathan Dotore <jonathandotoree@outlook.com>
2026-09-03 11:19:58 -0300

    [ADD] Adição da controller responsável pela rota de matrículas.

---
+++ EducaWebApi/Controllers/MatriculasController.cs
@@ -0,0 +1,30 @@
+using EducaWebApi.Data.Connection;
+using EducaWebApi.Data.Repositories;
+using EducaWebApi.Domain.Dtos;
+using EducaWebApi.Domain.Interfaces;
+using EducaWebApi.Service.Services;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace EducaWebApi.Controllers
+{
+    [RoutePrefix("api/matriculas")]
+    public class MatriculasController : ApiController
+    {
+        private readonly IMatriculaService _matriculaService;
+
+        public MatriculasController(IMatriculaService matriculaService)
+        {
+            _matriculaService = matriculaService;
+        }
+
+        public MatriculasController() : this(new MatriculaService(new MatriculaRepository(new SqlConnectionFactory()))) { }
+
+        [HttpPost, Route("")]
+        public async Task<IHttpActionResult> Matricular([FromBody] MatriculaRequestDto matricula)
+        {
+            var resposta = await _matriculaService.Matricular(matricula);
+            return Content(resposta.StatusCode, resposta);
+        }
+    }
+}
